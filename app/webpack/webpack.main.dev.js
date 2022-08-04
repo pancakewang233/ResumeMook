5a1d1c4a8445e68dac167e387ffb702684b340e6
pancake~ <834430775@qq.com>
2022-08-04 23:03:19 +0800

    electron+react项目init

---
+++ app/webpack/webpack.main.dev.js
@@ -0,0 +1,18 @@
+// 主进程开发环境配置
+
+const path = require('path');
+const baseConfig = require('./webpack.base.js');
+const webpackMerge = require('webpack-merge');
+
+const mainConfig = {
+  entry: path.resolve(__dirname, '../app/main/electron.js'),
+  target: 'electron-main',
+  output: {
+    filename: 'electron.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  devtool: 'inline-source-map',
+  mode: 'development',
+};
+
+module.exports = webpackMerge.merge(baseConfig, mainConfig);
